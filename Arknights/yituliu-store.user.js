2bda510481b609bacd927bcc320b67050875ccb9
Junbin <zjbthomas@gmail.com>
2023-11-14 15:01:42 -0800

    add sorting function

--- Arknights/yituliu-store.user.js
+++ Arknights/yituliu-store.user.js
@@ -7,6 +7,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=yituliu.site
 // @grant        none
 // @license      MIT
+// @namespace https://greasyfork.org/users/1217444
 // ==/UserScript==
 
 setTimeout(function(){
@@ -80,13 +81,84 @@ setTimeout(function(){
         }
     }
 
-    // for adding clear all button
-    var buttonDiv = document.createElement('div');
-    buttonDiv.style="width:100%;";
+    // function div
+    var functionDiv = document.createElement('div');
+    functionDiv.style="width:100%;display: flex; justify-content: flex-end; align-content: center;";
 
+    // sort label and checkbox
+    var sortLabel = document.createElement('label');
+    sortLabel.style="display: flex; align-items: center; margin-right:10px;";
+
+    var sortCheckbox = document.createElement('input');
+    sortCheckbox.type = "checkbox";
+    sortCheckbox.id = 'sort_checkbox';
+    sortCheckbox.style="margin-right:5px;";
+
+    sortLabel.addEventListener("click", function(e) {
+        var sortCheckbox = this.querySelector("#sort_checkbox");
+        if (e.target != sortCheckbox) return;
+
+        // not the best solution but at least working
+        if (e.target.checked) {
+            var sortedActContent = document.querySelector('#sorted_act_content');
+            if (sortedActContent == null) {
+                // create a new div withh sorted content
+                sortedActContent = document.createElement('div');
+                sortedActContent.id = "sorted_act_content";
+                sortedActContent.classList.add('act_content');
+
+                // get eff from all act cards
+                var effMap = new Map();
+                var actCards = document.querySelectorAll(".act_card:not(.uni_invisible)");
+                for (var i = 0; i < actCards.length ; i++) {
+                    if (actCards[i].innerHTML != "") {
+                        var effP = actCards[i].querySelector(".act_card_item_efficiency");
+                        if (effP != null) {
+                            var eff = parseFloat(effP.innerHTML);
+
+                            effMap.set(actCards[i], eff);
+                        }
+                    }
+                }
+
+                var sortedEffMap = new Map([...effMap.entries()].sort((a, b) => b[1] - a[1]));
+
+                // this moves node from .act_content to the newly created div; not the most elegant solution
+                for (var node of sortedEffMap.keys()) {
+                    sortedActContent.appendChild(node);
+                }
+
+                document.querySelector('.act_content:not(#sorted_act_content)').parentNode.insertBefore(sortedActContent, null);
+
+                // temporarily, remove all previous act contents
+                var actContents = document.querySelectorAll('.act_content:not(#sorted_act_content)');
+                for (var i = 0; i < actContents.length; i++) {
+                    actContents[i].remove();
+                }
+
+            }
+        } else {
+            // this just refreshes the page; not the best solution
+            location.reload();
+        }
+
+        // store in localStorage
+        localStorage.setItem("sorted", e.target.checked);
+    });
+
+    sortLabel.appendChild(sortCheckbox);
+    sortLabel.innerHTML += "合并后排序";
+
+    functionDiv.appendChild(sortLabel);
+
+    // read from localstorage
+    if (localStorage.getItem("sorted") != null && localStorage.getItem("sorted") === 'true') {
+        sortLabel.click();
+    }
+
+    // clear all button
     var button = document.createElement('button');
-    button.textContent = '一键清除';
-    button.style="width:100%;";
+    button.textContent = "清除所有";
 
     button.addEventListener("click", function(e) {
         var checkboxes = document.querySelectorAll(".redeem");
@@ -96,12 +168,11 @@ setTimeout(function(){
         }
     });
 
-    buttonDiv.appendChild(button);
+    functionDiv.appendChild(button);
 
     // insert before first act content
-    var firstActContent = document.querySelector('.act_content');
-
-    firstActContent.parentNode.insertBefore(buttonDiv, firstActContent);
+    var stageHint = document.querySelector('.stage_hint');
+    stageHint.parentNode.insertBefore(functionDiv, stageHint.nextSibling);
 
 }, 1000);
 
